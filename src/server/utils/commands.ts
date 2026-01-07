114327d9b015d43b0006325372deb50551d2a2b6
Rowan-Paul <rowan-paul@webbio.nl>
2026-01-07 10:29:27 +0100

    fix: fix escaping file names

--- src/server/utils/commands.ts
+++ src/server/utils/commands.ts
@@ -1,6 +1,6 @@
 export function escapeFilename(text: string): string {
   return text
-    .replace(/["\\]/g, "\\$&")
+    .replace(/["\\$`!]/g, "\\$&")
     .replace(/[/:*?"<>|]/g, "")
     .trim();
 }
